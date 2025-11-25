4da7cf90e2d4a925cde8164fa0137a15a83b7ea4
Shashank Shah <shashankshah@Shashanks-MacBook-Pro.local>
2025-11-25 17:44:40 +0530

    Fix

--- src/App.js
+++ src/App.js
@@ -2357,7 +2357,7 @@ function MainPanel(){
             </div>
         </div>
         <div class="contentBar">
-          <div style={{padding:"10px 0px 0px 20px"}}>
+          <div style={{padding:"10px 10px 15px 20px"}}>
             <h5 class="borderBottom"> 
               <FontAwesomeIcon icon={faChartBar} /> Graphs
             </h5>
